521148ce31240be76c3864b4ae9b5ffab4ff388d
escidron <eduardosanchezcidron@gmail.com>
2026-05-07 22:10:21 -0300

    fix vercel waf

--- app/config/settings.py
+++ app/config/settings.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     # Security
     SECRET_KEY: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 
     # Cookies
